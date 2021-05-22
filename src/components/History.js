1b31778ad142c757cb9fb5f19687589a74adbdde
Ana Laura <ana.simoes-matos@unesp.br>
2021-05-21 21:00:11 -0300

    historico

--- src/components/History.js
+++ src/components/History.js
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 
+
 export default function History(){
 
     return(
 
         <Container>
         <Title>Histórico</Title>
-        <span>
-        Em breve você poderá ver o histórico dos seus hábitos aqui!
-        </span>
+        <span>Em breve você poderá ver o histórico dos seus hábitos aqui!</span>
         </Container>
     )
 
@@ -18,13 +17,19 @@ const Container = styled.div`
 height: 100vh;
 width: 100vw; 
 margin-top:70px;
-background: #f2f2f2
+background: #f2f2f2;
 padding-left:17px;
 padding-right:17px;
+padding-top:28px;
 font-family: Lexend Deca;
+span{
+    font-family: Lexend Deca;
+font-size: 17.976px;
+color: #666666;
+}
 `;
 const Title = styled.div`
-margin-bottom:20px;
+margin-bottom:17px;
 font-size: 22.976px;
 color: #126BA5;
 display:flex;
